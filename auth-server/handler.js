96c24797219380d04192a16acc002a4f13d3eb11
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-01 21:58:00 +0200

    update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -104,3 +104,53 @@ module.exports.getAuthURL = async () => {
         });
     };
 
+module.exports.getCalendarEvents = event => {
+      const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+      );
+      const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+      oAuth2Client.setCredentials({ access_token });
+      
+        return new Promise((resolve, reject) => {
+
+        calendar.events.list(
+          {
+            calendarId: calendar_id,
+            auth: oAuth2Client,
+            timeMin: new Date().toISOString(),
+            singleEvents: true,
+            orderBy: "startTime",
+          },
+          (error, response) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(response);
+            }
+          }
+        );
+
+        })
+        .then((results) => {
+          return {
+            statusCode: 200,
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({ events: results.data.items })
+          };
+        })
+        .catch((err) => {
+          return {
+            statusCode: 500,
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify(err),
+          };
+        });
+
+      }
+

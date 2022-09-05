aba075e8f17e0fa968b97559d8d05753ddd4bc9a
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-05 09:06:46 +0200

    write test case scenario1

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,12 +1,18 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import App from '../App';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
 // import EventList from '../EventList';
 // import CitySearch from '../CitySearch';
 
-/*
-// Feature 1 - Scenario 1
+// Describe function; new group "scope" '<App /> component'
 describe('<App /> component', () => {
+    test('render list of events', () => {
+        const AppWrapper = shallow(<App />);
+        expect(AppWrapper.find(EventList)).toHaveLength(1);
+      });
+});
+/*
+
     let AppWrapper;
     beforeAll(() => {
         AppWrapper = shallow(<App />);

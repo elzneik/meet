199c2c976d19e9f78b5cbab3b458a1c2b8ec803f
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-05 09:25:53 +0200

    modify App.js and EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,20 +1,26 @@
-/*
 import React, { Component } from 'react';
 import Event from './Event';
 
 class EventList extends Component {
   render() {
     return (
-      <ul className="EventList">
+      <div>
+
+      </div>
+      
+    );
+  }
+}
+
+export default EventList;
+
+/*
+<ul className="EventList">
        {events.map(event =>
             <li key={event.id}>
                  <Event event={event} />
             </li>
         )}
       </ul>
-    );
-  }
-}
 
-export default EventList;
 */

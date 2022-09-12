a79ca7435df52e563e1a0bba5ee5ef3f2d8fc36d
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-12 19:26:27 +0200

    update

--- src/__tests__/EventList.Test.js
+++ src/__tests__/EventList.Test.js
@@ -7,6 +7,14 @@ import { mockData } from '../mock-data';
 import EventList from '../EventList';
 import Event from '../Event';
 
+    /* -------------------------------------------------------------------
+            FEATURE 1: FILTER EVENTS BY CITY
+    ---------------------------------------------------------------------*/
+
+    /* ----------------- SCENARIO 1 --------------------------------------: 
+    By default, when a user hasn’t searched for a city, show upcoming events 
+    from all locations.
+    ---------------------------------------------------------------------*/
 
 describe('<EventList /> component', () => {
   test('render correct number of events', () => {

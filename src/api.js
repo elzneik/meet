54f82b46de2d9d8306832452e233926c8f6ed2c2
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-16 13:08:03 +0200

    update api.js

--- src/api.js
+++ src/api.js
@@ -3,22 +3,10 @@ import { mockData } from "./mock-data";
 import axios from 'axios';
 
 /*
-* Access Token found in localStorage, check if token is valid, 
-* if not redirect to google authorization
-*/
-const checkToken = async (accessToken) => {
-  const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  )
-    .then((res) => res.json())
-    .catch((error) => error.json());
-
-  return result;
-};
-
-/*
-* No Access Token found on localStorage, then check Authorization code,
-* if no authorization code redirect to google
+* Check if there is token in storage
+* If token for access was not found on local storage
+* Check Authorization code,
+* If no authorization code redirect to google
 */
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
@@ -40,23 +28,50 @@ export const getAccessToken = async () => {
   return accessToken;
 }
 
-export const extractLocations = (events) => {
-    var extractLocations = events.map((event) => event.location);
-    var locations = [...new Set(extractLocations)];
-    return locations;
-  };
+/*
+* Check token vadility
+* Access Token found in localStorage, check if token is valid, 
+* if not redirect to google authorization
+*/
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
+};
+
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
 
 export const getEvents = async () => {
     NProgress.start();
+
     if (window.location.href.startsWith("http://localhost")) {
       NProgress.done();
       return mockData;
     }
   
     const token = await getAccessToken();
+
       if (token) {
         removeQuery();
-        const url = 'https://eajmc0egmg.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+        const url = "https://eajmc0egmg.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + 
+        "/" + token;
         const result = await axios.get(url);
         if (result.data) {
           var locations = extractLocations(result.data.events);
@@ -68,25 +83,12 @@ export const getEvents = async () => {
       }
     };
 
-    const removeQuery = () => {
-      if (window.history.pushState && window.location.pathname) {
-        var newurl =
-          window.location.protocol +
-          "//" +
-          window.location.host +
-          window.location.pathname;
-        window.history.pushState("", "", newurl);
-      } else {
-        newurl = window.location.protocol + "//" + window.location.host;
-        window.history.pushState("", "", newurl);
-      }
-    };
-
     const getToken = async (code) => {
       try {
           const encodeCode = encodeURIComponent(code);
   
-          const response = await fetch( 'https://eajmc0egmg.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
+          const response = await fetch('https://eajmc0egmg.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
+           '/' + encodeCode);
           if (!response.ok) {
               throw new Error(`HTTP error! status: ${response.status}`)
           }
@@ -96,5 +98,11 @@ export const getEvents = async () => {
       } catch(error) {
           error.json();
       }
+  };
+
+  export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
   };
 

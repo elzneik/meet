c8743a505f5829a2d1c18c0b39e795fa7c4c6192
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-20 17:14:39 +0200

    add code for offline data storage

--- src/api.js
+++ src/api.js
@@ -65,7 +65,13 @@ export const getEvents = async () => {
       NProgress.done();
       return mockData;
     }
-  
+
+    if (!navigator.onLine) {
+      const data = localStorage.getItem("lastEvents");
+      NProgress.done();
+      return data?JSON.parse(data).events:[];
+    }
+
     const token = await getAccessToken();
 
       if (token) {
